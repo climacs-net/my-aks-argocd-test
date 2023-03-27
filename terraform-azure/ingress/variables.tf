3623b39c8153edb42ff9c29223b49d9e257d2090
climacs-net <41995177+climacs-net@users.noreply.github.com>
2023-03-27 18:57:50 +0800

    Create variables.tf

---
+++ terraform-azure/ingress/variables.tf
@@ -0,0 +1,14 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure location for the resources"
+  type        = string
+}
+
+variable "aks_kubeconfig" {
+  description = "The AKS cluster kubeconfig"
+  type        = string
+}
