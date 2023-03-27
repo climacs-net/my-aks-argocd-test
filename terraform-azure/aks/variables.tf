99ec4ceb9c508676c6359588e5b9382ecf1b77ff
climacs-net <41995177+climacs-net@users.noreply.github.com>
2023-03-27 18:54:03 +0800

    Create variables.tf

---
+++ terraform-azure/aks/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure location for the resources"
+  type        = string
+}
